a8129c2d9059c008ec5d1fe6dc97336a4f7de0e1
John Thomas McDole <jtmcdole@gmail.com>
2016-02-16 22:23:48 -0800

    Add more props to ardknob_777.xml + --no-ard for debug sniffing

--- example/flightgear_777.dart
+++ example/flightgear_777.dart
@@ -24,6 +24,7 @@ import 'package:serial_port/serial_port.dart';
 DateTime _start = new DateTime.now();
 
 Logger log = new Logger('main');
+bool sniff = false;
 
 main(List<String> args) async {
   Logger.root.level = Level.ALL;
@@ -33,40 +34,44 @@ main(List<String> args) async {
   });
 
   log.info("start");
-  var port = args.isEmpty ? "COM3" : args.first;
-  port = new SerialPort(port, baudrate: 115200);
+  sniff = args.contains('--no-ard');
+  if (!sniff) {
+    var port = args.isEmpty ? "COM3" : args.first;
+    port = new SerialPort(port, baudrate: 115200);
 
-  try {
-    await port.open();
-  } catch (e) {
-    print("Error($e) listening to $port: ${e.stackTrace}");
-    exit(-1);
+    try {
+      await port.open();
+    } catch (e) {
+      print("Error($e) listening to $port: ${e.stackTrace}");
+      exit(-1);
+    }
+    log.info("$port");
+
+    var proto = new ArdProto(port);
+    log.info("$proto");
   }
-  log.info("$port");
-
-  var proto = new ArdProto(port);
-  log.info("$proto");
 
   var xml = await new File('example/ardknob_777.xml').readAsString();
   var props = new PropertyTree(xml);
   var bank = props['instrumentation/afds/inputs/bank-limit-switch'];
   var atl = props['instrumentation/afds/inputs/at-armed'];
   var atr = props['instrumentation/afds/inputs/at-armed[1]'];
-  proto.onAction.listen((KnobAction action) {
-    log.info(action);
-    if (action.id == 0) {
-      atl.value = action.direction == Direction.left ? 0 : 1;
-    }
-    if (action.id == 1) {
-      atr.value = action.direction == Direction.left ? 0 : 1;
-    }
-    if (action.id == 2) {
-      var val = bank.value;
-      val += (action.direction == Direction.left) ? -1 : 1;
-      bank.value = val;
-    }
-  });
-
+  if (!sniff) {
+    proto.onAction.listen((KnobAction action) {
+      log.info(action);
+      if (action.id == 0) {
+        atl.value = action.direction == Direction.left ? 0 : 1;
+      }
+      if (action.id == 1) {
+        atr.value = action.direction == Direction.left ? 0 : 1;
+      }
+      if (action.id == 2) {
+        var val = bank.value;
+        val += (action.direction == Direction.left) ? -1 : 1;
+        bank.value = val;
+      }
+    });
+  }
   var send = await RawDatagramSocket.bind(InternetAddress.ANY_IP_V4, 1236);
   props.onUpdate.listen((update) {
     log.info('sending: $update to 1235');
